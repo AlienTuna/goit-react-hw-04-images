e34606a14eebe8d4d0dc8c1665b7482fdf232ae9
AlienTuna <101868546+AlienTuna@users.noreply.github.com>
2023-08-15 13:05:53 +0300

    GIRP-4 +

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { toast } from 'react-toastify';
 
 import PixabayServices from "services/pixabay-services";
